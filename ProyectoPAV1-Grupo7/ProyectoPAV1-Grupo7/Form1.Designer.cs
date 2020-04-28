cb28f0181ebf2f6bfd87eee1c2a0f5a8e9db1b29
Walter Balderrama <71464@sistemas.frc.utn.edu.ar>
2020-04-28 20:41:17 -0300

    ?

--- ProyectoPAV1-Grupo7/ProyectoPAV1-Grupo7/Form1.Designer.cs
+++ ProyectoPAV1-Grupo7/ProyectoPAV1-Grupo7/Form1.Designer.cs
@@ -91,7 +91,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 161);
-            this.ControlBox = false;
             this.Controls.Add(this.btnIniciarSesion);
             this.Controls.Add(this.txtContrasena);
             this.Controls.Add(this.txtUsuario);
