8b66efbcfcbe8f366eb5c66fe88eab0f55d8c085
Walter Balderrama <71464@sistemasfrcutneduar.onmicrosoft.com>
2020-05-03 21:56:54 -0300

    mas forms

---
+++ ProyectoPAV1-Grupo7/ProyectoPAV1-Grupo7/Formularios/ABMs/frm_ABMProducto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPAV1_Grupo7.Formularios.ABMs
+{
+    public partial class frm_ABMProducto : Form
+    {
+        public frm_ABMProducto()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProyectoPAV1-Grupo7/ProyectoPAV1-Grupo7/Formularios/ABMs/frm_ABMSurtidor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPAV1_Grupo7.Formularios.ABMs
+{
+    public partial class frm_ABMSurtidor : Form
+    {
+        public frm_ABMSurtidor()
+        {
+            InitializeComponent();
+        }
+    }
+}
