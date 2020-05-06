e2df32c95646a59fb9621f3aceacdf52fe2de163
martinmicheletti <martinmicheletti@hotmail.com>
2020-05-05 23:46:08 -0300

    Update frm_ABMSurtidor.Designer.cs

--- ProyectoPAV1-Grupo7/ProyectoPAV1-Grupo7/Formularios/ABMs/frm_ABMSurtidor.Designer.cs
+++ ProyectoPAV1-Grupo7/ProyectoPAV1-Grupo7/Formularios/ABMs/frm_ABMSurtidor.Designer.cs
@@ -36,12 +36,18 @@
             this.cmbEstado = new System.Windows.Forms.ComboBox();
             this.cmbTipoCombustible = new System.Windows.Forms.ComboBox();
             this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 76);
+            this.label1.Location = new System.Drawing.Point(21, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 13);
             this.label1.TabIndex = 0;
@@ -50,7 +56,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(44, 106);
+            this.label2.Location = new System.Drawing.Point(21, 102);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 13);
             this.label2.TabIndex = 1;
@@ -59,7 +65,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(44, 133);
+            this.label3.Location = new System.Drawing.Point(21, 129);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(40, 13);
             this.label3.TabIndex = 2;
@@ -68,7 +74,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(44, 160);
+            this.label4.Location = new System.Drawing.Point(21, 156);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(103, 13);
             this.label4.TabIndex = 3;
@@ -77,7 +83,7 @@
             // cmbCuilEstacion
             // 
             this.cmbCuilEstacion.FormattingEnabled = true;
-            this.cmbCuilEstacion.Location = new System.Drawing.Point(181, 103);
+            this.cmbCuilEstacion.Location = new System.Drawing.Point(221, 99);
             this.cmbCuilEstacion.Name = "cmbCuilEstacion";
             this.cmbCuilEstacion.Size = new System.Drawing.Size(121, 21);
             this.cmbCuilEstacion.TabIndex = 4;
@@ -85,7 +91,7 @@
             // cmbEstado
             // 
             this.cmbEstado.FormattingEnabled = true;
-            this.cmbEstado.Location = new System.Drawing.Point(181, 130);
+            this.cmbEstado.Location = new System.Drawing.Point(221, 126);
             this.cmbEstado.Name = "cmbEstado";
             this.cmbEstado.Size = new System.Drawing.Size(121, 21);
             this.cmbEstado.TabIndex = 5;
@@ -93,14 +99,14 @@
             // cmbTipoCombustible
             // 
             this.cmbTipoCombustible.FormattingEnabled = true;
-            this.cmbTipoCombustible.Location = new System.Drawing.Point(181, 157);
+            this.cmbTipoCombustible.Location = new System.Drawing.Point(221, 153);
             this.cmbTipoCombustible.Name = "cmbTipoCombustible";
             this.cmbTipoCombustible.Size = new System.Drawing.Size(121, 21);
             this.cmbTipoCombustible.TabIndex = 6;
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Location = new System.Drawing.Point(416, 307);
+            this.btnCerrar.Location = new System.Drawing.Point(267, 326);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(75, 23);
             this.btnCerrar.TabIndex = 7;
@@ -108,8 +114,11 @@
             this.btnCerrar.UseVisualStyleBackColor = true;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 =======
+=======
+>>>>>>> master
             // btnEliminar
             // 
             this.btnEliminar.Location = new System.Drawing.Point(186, 326);
@@ -136,7 +145,10 @@
             this.btnGuardar.TabIndex = 30;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+<<<<<<< HEAD
             this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+=======
+>>>>>>> master
             // 
             // dataGridView1
             // 
@@ -153,12 +165,20 @@
             this.textBox1.Size = new System.Drawing.Size(121, 20);
             this.textBox1.TabIndex = 34;
             // 
+<<<<<<< HEAD
 >>>>>>> Stashed changes
+=======
+>>>>>>> master
             // frm_ABMSurtidor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.cmbTipoCombustible);
             this.Controls.Add(this.cmbEstado);
@@ -170,6 +190,7 @@
             this.Name = "frm_ABMSurtidor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frm_ABMSurtidor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,5 +206,10 @@
         private System.Windows.Forms.ComboBox cmbEstado;
         private System.Windows.Forms.ComboBox cmbTipoCombustible;
         private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
