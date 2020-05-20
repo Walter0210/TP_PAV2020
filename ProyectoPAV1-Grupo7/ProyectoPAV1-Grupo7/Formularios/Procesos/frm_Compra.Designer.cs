9f3ca5c5c5558ce577f1938ab9b74c8173c225c4
dlizarralde2020 <dlizarralde2020@hotmail.com>
2020-05-20 16:11:44 -0300

    compra cambios

--- ProyectoPAV1-Grupo7/ProyectoPAV1-Grupo7/Formularios/Procesos/frm_Compra.Designer.cs
+++ ProyectoPAV1-Grupo7/ProyectoPAV1-Grupo7/Formularios/Procesos/frm_Compra.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.cmbEstacion = new System.Windows.Forms.ComboBox();
             this.cmbSurtidor = new System.Windows.Forms.ComboBox();
-            this.txtBoxCantidad = new System.Windows.Forms.TextBox();
+            this.tbCantidad = new System.Windows.Forms.TextBox();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.cmbUnidadMedida = new System.Windows.Forms.ComboBox();
             this.lblResponsable = new System.Windows.Forms.Label();
@@ -38,7 +38,7 @@
             this.lblCantidad = new System.Windows.Forms.Label();
             this.lblObservaciones = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tbObservaciones = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // cmbEstacion
@@ -48,9 +48,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cmbEstacion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbEstacion.FormattingEnabled = true;
-            this.cmbEstacion.Location = new System.Drawing.Point(178, 85);
+            this.cmbEstacion.Location = new System.Drawing.Point(237, 105);
+            this.cmbEstacion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbEstacion.Name = "cmbEstacion";
-            this.cmbEstacion.Size = new System.Drawing.Size(215, 21);
+            this.cmbEstacion.Size = new System.Drawing.Size(285, 24);
             this.cmbEstacion.TabIndex = 0;
             // 
             // cmbSurtidor
@@ -60,28 +61,29 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cmbSurtidor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbSurtidor.FormattingEnabled = true;
-            this.cmbSurtidor.Location = new System.Drawing.Point(178, 115);
+            this.cmbSurtidor.Location = new System.Drawing.Point(237, 142);
+            this.cmbSurtidor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbSurtidor.Name = "cmbSurtidor";
-            this.cmbSurtidor.Size = new System.Drawing.Size(215, 21);
+            this.cmbSurtidor.Size = new System.Drawing.Size(285, 24);
             this.cmbSurtidor.TabIndex = 1;
             // 
-            // txtBoxCantidad
+            // tbCantidad
             // 
-            this.txtBoxCantidad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.tbCantidad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.txtBoxCantidad.Location = new System.Drawing.Point(178, 147);
-            this.txtBoxCantidad.Name = "txtBoxCantidad";
-            this.txtBoxCantidad.Size = new System.Drawing.Size(90, 20);
-            this.txtBoxCantidad.TabIndex = 2;
+            this.tbCantidad.Location = new System.Drawing.Point(237, 181);
+            this.tbCantidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbCantidad.Name = "tbCantidad";
+            this.tbCantidad.Size = new System.Drawing.Size(119, 22);
+            this.tbCantidad.TabIndex = 2;
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(20, 9);
-            this.lblTitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitulo.Location = new System.Drawing.Point(27, 11);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(248, 25);
+            this.lblTitulo.Size = new System.Drawing.Size(305, 29);
             this.lblTitulo.TabIndex = 62;
             this.lblTitulo.Text = "VENTA DE PRODUCTO";
             // 
@@ -92,9 +94,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cmbUnidadMedida.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbUnidadMedida.FormattingEnabled = true;
-            this.cmbUnidadMedida.Location = new System.Drawing.Point(276, 147);
+            this.cmbUnidadMedida.Location = new System.Drawing.Point(368, 181);
+            this.cmbUnidadMedida.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbUnidadMedida.Name = "cmbUnidadMedida";
-            this.cmbUnidadMedida.Size = new System.Drawing.Size(116, 21);
+            this.cmbUnidadMedida.Size = new System.Drawing.Size(153, 24);
             this.cmbUnidadMedida.TabIndex = 64;
             // 
             // lblResponsable
@@ -103,9 +106,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblResponsable.AutoSize = true;
-            this.lblResponsable.Location = new System.Drawing.Point(43, 85);
+            this.lblResponsable.Location = new System.Drawing.Point(57, 105);
+            this.lblResponsable.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblResponsable.Name = "lblResponsable";
-            this.lblResponsable.Size = new System.Drawing.Size(51, 13);
+            this.lblResponsable.Size = new System.Drawing.Size(66, 17);
             this.lblResponsable.TabIndex = 65;
             this.lblResponsable.Text = "Estacion:";
             this.lblResponsable.Click += new System.EventHandler(this.lblResponsable_Click);
@@ -116,9 +120,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 118);
+            this.label1.Location = new System.Drawing.Point(57, 145);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(62, 17);
             this.label1.TabIndex = 66;
             this.label1.Text = "Surtidor:";
             // 
@@ -128,9 +133,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblCantidad.AutoSize = true;
-            this.lblCantidad.Location = new System.Drawing.Point(43, 151);
+            this.lblCantidad.Location = new System.Drawing.Point(57, 186);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(94, 13);
+            this.lblCantidad.Size = new System.Drawing.Size(124, 17);
             this.lblCantidad.TabIndex = 67;
             this.lblCantidad.Text = "Cantidad Vendida:";
             this.lblCantidad.Click += new System.EventHandler(this.label2_Click);
@@ -141,9 +147,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblObservaciones.AutoSize = true;
-            this.lblObservaciones.Location = new System.Drawing.Point(41, 186);
+            this.lblObservaciones.Location = new System.Drawing.Point(55, 229);
+            this.lblObservaciones.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblObservaciones.Name = "lblObservaciones";
-            this.lblObservaciones.Size = new System.Drawing.Size(81, 13);
+            this.lblObservaciones.Size = new System.Drawing.Size(107, 17);
             this.lblObservaciones.TabIndex = 76;
             this.lblObservaciones.Text = "Observaciones:";
             // 
@@ -151,30 +158,33 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(150, 449);
+            this.button1.Location = new System.Drawing.Point(200, 553);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.Size = new System.Drawing.Size(199, 28);
             this.button1.TabIndex = 77;
             this.button1.Text = "Registrar venta";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
+            // tbObservaciones
             // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.tbObservaciones.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(46, 213);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(347, 202);
-            this.richTextBox1.TabIndex = 78;
-            this.richTextBox1.Text = "";
+            this.tbObservaciones.Location = new System.Drawing.Point(61, 262);
+            this.tbObservaciones.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbObservaciones.Name = "tbObservaciones";
+            this.tbObservaciones.Size = new System.Drawing.Size(461, 248);
+            this.tbObservaciones.TabIndex = 78;
+            this.tbObservaciones.Text = "";
             // 
             // frm_Compra
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 484);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(591, 596);
+            this.Controls.Add(this.tbObservaciones);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblObservaciones);
             this.Controls.Add(this.lblCantidad);
@@ -182,12 +192,14 @@
             this.Controls.Add(this.lblResponsable);
             this.Controls.Add(this.cmbUnidadMedida);
             this.Controls.Add(this.lblTitulo);
-            this.Controls.Add(this.txtBoxCantidad);
+            this.Controls.Add(this.tbCantidad);
             this.Controls.Add(this.cmbSurtidor);
             this.Controls.Add(this.cmbEstacion);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "frm_Compra";
             this.Text = "frm_Compra";
+            this.Load += new System.EventHandler(this.frm_Compra_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,7 +209,7 @@
 
         private System.Windows.Forms.ComboBox cmbEstacion;
         private System.Windows.Forms.ComboBox cmbSurtidor;
-        private System.Windows.Forms.TextBox txtBoxCantidad;
+        private System.Windows.Forms.TextBox tbCantidad;
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.ComboBox cmbUnidadMedida;
         private System.Windows.Forms.Label lblResponsable;
@@ -205,6 +217,6 @@
         private System.Windows.Forms.Label lblCantidad;
         private System.Windows.Forms.Label lblObservaciones;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox tbObservaciones;
     }
 }
